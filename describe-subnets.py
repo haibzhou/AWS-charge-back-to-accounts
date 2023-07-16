63395473619c32ed4eb15f94c281f2f68d44a788
Haibin Zhou <109695471+haibzhou@users.noreply.github.com>
2023-07-16 10:36:24 -0400

    Add files via upload

--- describe-subnets.py
+++ describe-subnets.py
@@ -2,27 +2,29 @@
 
 import boto3
 def list_subnets(profile, region):
+
     session = boto3.Session(profile_name=profile)
     ec2=session.resource('ec2', region_name=region)
-    for subnet in ec2.subnets.all():
-        print(region, subnet.vpc_id, subnet.availability_zone_id, subnet.id, subnet.cidr_block, subnet.owner_id)
-#        print(subnet.availability_zone_id)
-#        print(subnet.vpc_id)
-#        print(subnet.owner_id)
-#        #print subnet tag key Name and value
-#        print(subnet.tags)
-
-       
-        
+    for subnet in ec2.subnets.all():  
+        print(region, subnet.vpc_id, subnet.availability_zone_id, subnet.id, subnet.cidr_block, subnet.owner_id) 
+        subnet = subnet.owner_id+',' +region+','+ subnet.vpc_id+','+ subnet.availability_zone_id+','+ subnet.id+','+ subnet.cidr_block+ '\n'
+        file.write(subnet)
+            
 
  #main program
 if __name__ == '__main__':
-    list_subnets('network', 'us-east-1')
+    #open file for writing
+    file = open("describe-subnets.txt", "w")
+    file.write('owner_id,region,vpc_id,availability_zone_id,subnet_id,cidr_block\n')
+
+    list_subnets('network', 'us-east-1', )
     list_subnets('network', 'us-west-2')
     list_subnets('sales', 'us-east-1')
     list_subnets('sales', 'us-west-2')
     list_subnets('hr', 'us-east-1')
     list_subnets('hr', 'us-west-2')
+    
+    file.close()
 
 
 
