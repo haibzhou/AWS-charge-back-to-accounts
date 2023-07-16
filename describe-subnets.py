d8629431777cf9c205848d5cd1cfc305ca50219d
Haibin Zhou <109695471+haibzhou@users.noreply.github.com>
2023-07-16 10:15:29 -0400

    Add files via upload

--- describe-subnets.py
+++ describe-subnets.py
@@ -5,9 +5,15 @@ def list_subnets(profile, region):
     session = boto3.Session(profile_name=profile)
     ec2=session.resource('ec2', region_name=region)
     for subnet in ec2.subnets.all():
-        print(subnet.id, subnet.cidr_block)
-        print(subnet.availability_zone_id)
-        print(subnet.vpc_id)
+        print(region, subnet.vpc_id, subnet.availability_zone_id, subnet.id, subnet.cidr_block, subnet.owner_id)
+#        print(subnet.availability_zone_id)
+#        print(subnet.vpc_id)
+#        print(subnet.owner_id)
+#        #print subnet tag key Name and value
+#        print(subnet.tags)
+
+       
+        
 
  #main program
 if __name__ == '__main__':
